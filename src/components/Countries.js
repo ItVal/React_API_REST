fd79537657d7c916ccaea7280abcaef767bf72d9
ItVal <valentinhonvj1@gmail.com>
2022-07-30 16:06:01 +0200

    feat : bouton radio pour chaque continent

---
+++ src/components/Countries.js
@@ -0,0 +1,60 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Card from "./Card";
+
+const Countries = () => {
+    // pour affichage de pays
+  const [data, setData] = useState([]);
+    // progressbar determinant les nombres de pays à afficher
+  const [rangeValue, setRangeValue] = useState(36)
+    // pour filtrer les pays par continents
+  const [selectedRadio, setSelectedRadio] = useState("");
+    // Pour le bouton radio pour chaque continent
+  const radios = ["Africa", "America", "Asia", "Europe", "Oceania"]
+
+  // le useEffect se joue lorque le composant est monté
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((res) => setData(res.data));
+  }, []);
+
+  return (
+    <div className="countries">
+      {/* progressBar */}
+      <ul className="radio-container">
+        <input 
+        type="range" 
+        min="1" 
+        max="250" 
+        defaultValue={rangeValue} 
+        onChange={(e) => setRangeValue(e.target.value)} />
+      
+      {/* input de type radio pour ranger les pays par continent */}
+      {radios.map((continent) => ( 
+         <li>
+            <input 
+            type="radio" 
+            id={continent} 
+            name="continentRadio" 
+            onChange={(e) => setSelectedRadio(e.target.id)} />
+            <label htmlFor={continent}>{continent}</label>
+         </li>
+         ))}
+     </ul>
+      {/* affichage de pays */}
+      <ul>
+        {/* filter (pour filtrer le pays par continent), sort (pour trier par nombre de population), la méthode .slice determiner le nombre de pays à afficher sur la page principal, .map il nous permet d'afficher tout les contenus de notre tableau pays */}
+        {data
+        .filter((country) => country.continents[0].includes(selectedRadio))
+        .sort((a, b) =>  b.population - a.population)
+        .slice(0, rangeValue) 
+        .map((country, index) => (
+          <Card key={index} country={country} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Countries;
---
+++ src/pages/About.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />  
+      <Navigation />  
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, ullam
+        unde. Optio, exercitationem amet voluptates illo odit aut soluta qui
+        dolorem vitae impedit cumque sit laborum nam neque rerum veritatis.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates
+        sapiente beatae dolorem libero aspernatur non reiciendis incidunt, nisi
+        illum ad ducimus eaque, cum error. Nobis nostrum laborum aperiam
+        repellendus commodi rem, corporis iure mollitia porro deserunt fugit
+        beatae, consequuntur dignissimos ratione cum veniam officia qui,
+        praesentium enim necessitatibus exercitationem dolores. Esse nihil
+        aliquam at ab ipsum maiores rerum nostrum voluptatem quae deleniti qui
+        nisi nam, reprehenderit illo cumque, tempora amet sequi quas unde,
+        praesentium assumenda. Rerum laudantium unde quam, quod, beatae fuga,
+        iste ipsum id non expedita excepturi saepe pariatur optio minus
+        praesentium incidunt. Similique corrupti eveniet assumenda, nobis
+        debitis dolor necessitatibus odio ipsum minus, ex deserunt veritatis?
+        Eligendi commodi reiciendis quaerat consequatur, ea maxime cum ipsam
+        numquam voluptate fuga saepe laboriosam quo! Ratione nostrum, odio
+        consequuntur tempora explicabo recusandae blanditiis assumenda,
+        praesentium, temporibus atque veritatis obcaecati nobis illum dolorum
+        amet et laboriosam maxime voluptatum quisquam voluptate animi
+        distinctio. Nemo quos hic culpa? Aliquid molestiae, at error incidunt
+        laborum adipisci dolorem beatae tempora, iure quis corporis officiis
+        architecto explicabo nihil quam temporibus? Odit quaerat, quasi
+        reprehenderit officiis repellat quibusdam dolorum, earum voluptas
+        debitis perferendis consectetur non impedit voluptatem. Inventore nihil
+        repellendus est laboriosam tenetur eligendi, consectetur doloribus rerum
+        consequatur consequuntur!
+      </p>
+    </div>
+  );
+};
+
+export default About;
